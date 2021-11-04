ddebf3ae5f2a9d00925ee46ac61afc0f97ae8a24
archer1987 <nikolos04051987@gmail.com>
2021-11-04 21:02:16 +0700

    lesson__3

--- project/js/main.js
+++ project/js/main.js
@@ -1,25 +1,93 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000, pic:'note.jpg'},
-    {id: 2, title: 'Mouse', price: 20,pic:'mouse.jpg'},
-    {id: 3, title: 'Keyboard', price: 200,pic:'keyboard.jpg'},
-    {id: 4, title: 'Gamepad', price: 50, pic:'geympad.jpg'},
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (item) => {
-    return `<div class="product-item">
-                <h3>${item.title}</h3>
-                <p>${item.price}</p>
-                <img src="${item.pic}" width="150" height="120">
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
-const renderPage = list => {
-    let productsList = list.map(item => renderProduct(item));
-    console.log(...productsList);
-    
-    document.querySelector('.products').innerHTML =productsList.join('');
-};
+// const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-renderPage(products);
+// // let getRequest = (url, cb) => {
+// //     let xhr = new XMLHttpRequest();
+// //     // window.ActiveXObject -> xhr = new ActiveXObject()
+// //     xhr.open("GET", url, true);
+// //     xhr.onreadystatechange = () => {
+// //         if(xhr.readyState === 4){
+// //             if(xhr.status !== 200){
+// //                 console.log('Error');
+// //             } else {
+// //                 cb(xhr.responseText);
+// //             }
+// //         }
+// //     };
+// //     xhr.send();
+// // };
 
+// class ProductsList {
+//     constructor(container = '.products'){
+//         this.container = container;
+//         this.goods = [];//массив товаров из JSON документа
+//         this._getProducts()
+//             .then(data => { //data - объект js
+//                  this.goods = data;
+//                  this.render()
+//             });
+//     }
+//     // _fetchProducts(cb){
+//     //     getRequest(`${API}/catalogData.json`, (data) => {
+//     //         this.goods = JSON.parse(data);
+//     //         console.log(this.goods);
+//     //         cb();
+//     //     })
+//     // }
+//     _get
+//     Products(){
+      
+//         return fetch(`${API}/catalogData.json`)
+//             .then(result => result.json())
+//             .catch(error => {
+//                 console.log(error);
+//             });
+       
+//     }
+//     calcSum(){
+//         return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+//     }
+//     render(){
+//         const block = document.querySelector(this.container);
+//         for (let product of this.goods){
+//             const productObj = new ProductItem(product);
+// //            this.allProducts.push(productObj);
+//             block.insertAdjacentHTML('beforeend', productObj.render());
+//         }
+
+//     }
+// }
+
+
+// class ProductItem {
+//     constructor(product, img = 'https://via.placeholder.com/200x150'){
+//         this.title = product.product_name;
+//         this.price = product.price;
+//         this.id = product.id_product;
+//         this.img = img;
+//     }
+//     render(){
+//         return `<div class="product-item" data-id="${this.id}">
+//                 <img src="${this.img}" alt="Some img">
+//                 <div class="desc">
+//                     <h3>${this.title}</h3>
+//                     <p>${this.price} $</p>
+//                     <button class="buy-btn">Купить</button>
+//                 </div>
+//             </div>`
+//     }
+// }
+
+
+    function addsProduct(){
+        let add=document.querySelector(".buy-btn")
+        console.log(add)
+    }
+
+
+
+
+// // let list = new ProductsList();
+// console.log(list.allProducts);
+
+
+addsProduct()
